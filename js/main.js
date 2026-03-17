1ed82ec664656ffdeb02367b960e3d9b644d0d66
Hexbob6 <sam.walker.email@gmail.com>
2026-03-17 00:48:18 +0000

    Re-adding files to rework blurring and add favicon logic
    Blurring is now adaptive between mobile and desktop (mobile was too blurred before). Code and files have also been added to change to a red favicon when the page is moved away. This needs testing as doesn't work in vscode preview.

--- js/main.js
+++ js/main.js
@@ -3,3 +3,15 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log("Portfolio ready");
 
 });
+
+const favicon = document.getElementById("favicon");
+
+document.addEventListener("visibilitychange", () => {
+
+    const state = document.hidden ? "-away" : "";
+
+    favicon.setAttribute("href", `icons/favicon${state}.png`);
+
+});
+
+
